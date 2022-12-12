fb3acfdb194347cd6fd6671222bd4defd86f5a48
cuza91 <cuza91@gmail.com>
2022-12-12 00:41:49 -0500

    agregada funcion de contar

--- Assets/Js/js.js
+++ Assets/Js/js.js
@@ -1,5 +1,19 @@
 function calcular() {
-   
 
-  
+    var a = parseFloat(document.getElementById("b1000").value * 1000);
+    var b = parseFloat(document.getElementById("b10").value * 10);
+    var c = parseFloat(document.getElementById("b500").value * 500);
+    var d = parseFloat(document.getElementById("b5").value * 5);
+    var e = parseFloat(document.getElementById("b200").value * 200);
+    var f = parseFloat(document.getElementById("b3").value * 3);
+    var g = parseFloat(document.getElementById("b100").value * 100);
+    var h = parseFloat(document.getElementById("b1").value * 1);
+    var i = parseFloat(document.getElementById("b50").value * 50);
+    var j = parseFloat(document.getElementById("b0.2").value * 0.2);
+    var k = parseFloat(document.getElementById("b20").value * 20);
+    var l = parseFloat(document.getElementById("b0.05").value * 0.05);
+
+    document.getElementById("pcup").value = a + b + c + d + e + f + g + h + i + j + k + l +" cup";
+
+
 };
