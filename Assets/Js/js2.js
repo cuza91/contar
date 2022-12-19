1815d55943735ee98f4d28708e19481699f10cfb
cuza91 <cuza91@gmail.com>
2022-12-18 23:39:48 -0500

    limpiar

--- Assets/Js/js2.js
+++ Assets/Js/js2.js
@@ -9,3 +9,43 @@ function usd1() {
     document.getElementById("sectionm1").style.display = "none";
 
 };
+
+
+function limpiarMn1() {
+    document.getElementById("m1000").value = "";
+    document.getElementById("m10").value = "";
+    document.getElementById("m500").value = "";
+    document.getElementById("m5").value = "";
+    document.getElementById("m200").value = "";
+    document.getElementById("m3").value = "";
+    document.getElementById("m100").value = "";
+    document.getElementById("m1").value = "";
+    document.getElementById("m50").value = "";
+    document.getElementById("m0.20").value = "";
+    document.getElementById("m20").value = "";
+    document.getElementById("m0.05").value = "";
+    document.getElementById("pcup1").value = "";
+    document.getElementById("tcup1").value = "";
+    document.getElementById("tusd1").value = "";
+
+
+};
+
+function limpiarUsd1() {
+    document.getElementById("i1000").value = "";
+    document.getElementById("i10").value = "";
+    document.getElementById("i500").value = "";
+    document.getElementById("i5").value = "";
+    document.getElementById("i200").value = "";
+    document.getElementById("i3").value = "";
+    document.getElementById("i100").value = "";
+    document.getElementById("i1").value = "";
+    document.getElementById("i50").value = "";
+    document.getElementById("i0.20").value = "";
+    document.getElementById("i20").value = "";
+    document.getElementById("i0.05").value = "";
+    document.getElementById("pusd1").value = "";
+    document.getElementById("tcup1").value = "";
+    document.getElementById("tusd1").value = "";
+
+};
