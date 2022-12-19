6f72e4e0e027b8ab56c0a8a5a67dc86b866553ed
cuza91 <cuza91@gmail.com>
2022-12-18 23:15:42 -0500

    maquetacion terminada

---
+++ Assets/Js/js2.js
@@ -0,0 +1,11 @@
+function cup1() {
+    document.getElementById("sectionm1").style.display = "contents";
+    document.getElementById("sectionm2").style.display = "none";
+
+};
+
+function usd1() {
+    document.getElementById("sectionm2").style.display = "contents";
+    document.getElementById("sectionm1").style.display = "none";
+
+};
