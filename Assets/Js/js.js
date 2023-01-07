fe42429447638d806107d24a61ecc4132555721d
cuza91 <cuza91@gmail.com>
2023-01-07 09:26:07 -0500

    codigo formateado

--- Assets/Js/js.js
+++ Assets/Js/js.js
@@ -1,69 +1,44 @@
 function calcularmn() {
-    document.getElementById("pcup").value = sabermn().toFixed(2) + " CUP";
-    document.getElementById("pusd").value = saberusd().toFixed(2) + " USD";
+  document.getElementById("pcup").value = sabermn().toFixed(2) + " CUP";
+  document.getElementById("pusd").value = saberusd().toFixed(2) + " USD";
 
-    if (document.getElementById("tasa-cambio").value != "") {
-        document.getElementById("tcup").value = totalcup().toFixed(2) + " CUP";
-        document.getElementById("tusd").value = totalusd().toFixed(2) + " USD";
-    }
-
-    else {
-        document.getElementById("tcup").value = "";
-        document.getElementById("tusd").value = "";
-    }
-
-};
+  if (document.getElementById("tasa-cambio").value != "") {
+    document.getElementById("tcup").value = totalcup().toFixed(2) + " CUP";
+    document.getElementById("tusd").value = totalusd().toFixed(2) + " USD";
+  } else {
+    document.getElementById("tcup").value = "";
+    document.getElementById("tusd").value = "";
+  }
+}
 
 function calcularusd() {
-    document.getElementById("pusd").value = saberusd().toFixed(2) + " USD";
-    document.getElementById("pcup").value = sabermn().toFixed(2) + " CUP";
+  document.getElementById("pusd").value = saberusd().toFixed(2) + " USD";
+  document.getElementById("pcup").value = sabermn().toFixed(2) + " CUP";
 
-    if (document.getElementById("tasa-cambio").value != "") {
-        document.getElementById("tcup").value = totalcup().toFixed(2) + " CUP";
-        document.getElementById("tusd").value = totalusd().toFixed(2) + " USD";
-    }
-
-    else {
-        document.getElementById("tcup").value = "";
-        document.getElementById("tusd").value = "";
-    }
-
-};
-
-
-
-
-
-
+  if (document.getElementById("tasa-cambio").value != "") {
+    document.getElementById("tcup").value = totalcup().toFixed(2) + " CUP";
+    document.getElementById("tusd").value = totalusd().toFixed(2) + " USD";
+  } else {
+    document.getElementById("tcup").value = "";
+    document.getElementById("tusd").value = "";
+  }
+}
 
 function cup() {
-    document.getElementById("section1").style.display = "contents";
-    document.getElementById("section2").style.display = "none";
-
-};
+  document.getElementById("section1").style.display = "contents";
+  document.getElementById("section2").style.display = "none";
+}
 
 function usd() {
-    document.getElementById("section2").style.display = "contents";
-    document.getElementById("section1").style.display = "none";
-
-};
-
+  document.getElementById("section2").style.display = "contents";
+  document.getElementById("section1").style.display = "none";
+}
 
 function tasacambio() {
-    if (document.getElementById("tc").innerHTML == "Usar Tasa de Cambio Manual") {
-       document.getElementById("tc").innerHTML = "Usar Tasa de Cambio del Toque";
-       document.getElementById("tasa-cambio").value = "";
-      
-        
-    }
-
-     else {
-        document.getElementById("tc").innerHTML = "Usar Tasa de Cambio Manual";
-       
-    }
-
-};
-
-
-
-
+  if (document.getElementById("tc").innerHTML == "Usar Tasa de Cambio Manual") {
+    document.getElementById("tc").innerHTML = "Usar Tasa de Cambio del Toque";
+    document.getElementById("tasa-cambio").value = "";
+  } else {
+    document.getElementById("tc").innerHTML = "Usar Tasa de Cambio Manual";
+  }
+}
--- Assets/Js/js2.js
+++ Assets/Js/js2.js
@@ -1,142 +1,126 @@
 function cup1() {
-    document.getElementById("sectionm1").style.display = "contents";
-    document.getElementById("sectionm2").style.display = "none";
-
-};
+  document.getElementById("sectionm1").style.display = "contents";
+  document.getElementById("sectionm2").style.display = "none";
+}
 
 function usd1() {
-    document.getElementById("sectionm2").style.display = "contents";
-    document.getElementById("sectionm1").style.display = "none";
-
-};
-
+  document.getElementById("sectionm2").style.display = "contents";
+  document.getElementById("sectionm1").style.display = "none";
+}
 
 function limpiarMn1() {
-    document.getElementById("m1000").value = "";
-    document.getElementById("m10").value = "";
-    document.getElementById("m500").value = "";
-    document.getElementById("m5").value = "";
-    document.getElementById("m200").value = "";
-    document.getElementById("m3").value = "";
-    document.getElementById("m100").value = "";
-    document.getElementById("m1").value = "";
-    document.getElementById("m50").value = "";
-    document.getElementById("m0.20").value = "";
-    document.getElementById("m20").value = "";
-    document.getElementById("m0.05").value = "";
-    document.getElementById("pcup1").value = "";
-    document.getElementById("tcup1").value = "";
-    document.getElementById("tusd1").value = "";
-
-
-};
+  document.getElementById("m1000").value = "";
+  document.getElementById("m10").value = "";
+  document.getElementById("m500").value = "";
+  document.getElementById("m5").value = "";
+  document.getElementById("m200").value = "";
+  document.getElementById("m3").value = "";
+  document.getElementById("m100").value = "";
+  document.getElementById("m1").value = "";
+  document.getElementById("m50").value = "";
+  document.getElementById("m0.20").value = "";
+  document.getElementById("m20").value = "";
+  document.getElementById("m0.05").value = "";
+  document.getElementById("pcup1").value = "";
+  document.getElementById("tcup1").value = "";
+  document.getElementById("tusd1").value = "";
+}
 
 function limpiarUsd1() {
-    document.getElementById("i1000").value = "";
-    document.getElementById("i10").value = "";
-    document.getElementById("i500").value = "";
-    document.getElementById("i5").value = "";
-    document.getElementById("i200").value = "";
-    document.getElementById("i3").value = "";
-    document.getElementById("i100").value = "";
-    document.getElementById("i1").value = "";
-    document.getElementById("i50").value = "";
-    document.getElementById("i0.20").value = "";
-    document.getElementById("i20").value = "";
-    document.getElementById("i0.05").value = "";
-    document.getElementById("pusd1").value = "";
-    document.getElementById("tcup1").value = "";
-    document.getElementById("tusd1").value = "";
-
-};
-
+  document.getElementById("i1000").value = "";
+  document.getElementById("i10").value = "";
+  document.getElementById("i500").value = "";
+  document.getElementById("i5").value = "";
+  document.getElementById("i200").value = "";
+  document.getElementById("i3").value = "";
+  document.getElementById("i100").value = "";
+  document.getElementById("i1").value = "";
+  document.getElementById("i50").value = "";
+  document.getElementById("i0.20").value = "";
+  document.getElementById("i20").value = "";
+  document.getElementById("i0.05").value = "";
+  document.getElementById("pusd1").value = "";
+  document.getElementById("tcup1").value = "";
+  document.getElementById("tusd1").value = "";
+}
 
 function calcularmn1() {
-    document.getElementById("pcup1").value = sabermn1().toFixed(2) + " CUP";
-    document.getElementById("pusd1").value = saberusd1().toFixed(2) + " USD";
+  document.getElementById("pcup1").value = sabermn1().toFixed(2) + " CUP";
+  document.getElementById("pusd1").value = saberusd1().toFixed(2) + " USD";
 
-    if (document.getElementById("tasa2").value != "") {
-        document.getElementById("tcup1").value = totalcup1().toFixed(2) + " CUP";
-        document.getElementById("tusd1").value = totalusd1().toFixed(2) + " USD";
-    }
-
-    else {
-        document.getElementById("tcup1").value = "";
-        document.getElementById("tusd1").value = "";
-    }
-
-};
+  if (document.getElementById("tasa2").value != "") {
+    document.getElementById("tcup1").value = totalcup1().toFixed(2) + " CUP";
+    document.getElementById("tusd1").value = totalusd1().toFixed(2) + " USD";
+  } else {
+    document.getElementById("tcup1").value = "";
+    document.getElementById("tusd1").value = "";
+  }
+}
 
 function calcularusd1() {
-    document.getElementById("pusd1").value = saberusd1().toFixed(2) + " USD";
-    document.getElementById("pcup1").value = sabermn1().toFixed(2) + " CUP";
+  document.getElementById("pusd1").value = saberusd1().toFixed(2) + " USD";
+  document.getElementById("pcup1").value = sabermn1().toFixed(2) + " CUP";
 
-    if (document.getElementById("tasa2").value != "") {
-        document.getElementById("tcup1").value = totalcup1().toFixed(2) + " CUP";
-        document.getElementById("tusd1").value = totalusd1().toFixed(2) + " USD";
-    }
-
-    else {
-        document.getElementById("tcup1").value = "";
-        document.getElementById("tusd1").value = "";
-    }
-
-};
-
-
-
+  if (document.getElementById("tasa2").value != "") {
+    document.getElementById("tcup1").value = totalcup1().toFixed(2) + " CUP";
+    document.getElementById("tusd1").value = totalusd1().toFixed(2) + " USD";
+  } else {
+    document.getElementById("tcup1").value = "";
+    document.getElementById("tusd1").value = "";
+  }
+}
 
 function sabermn1() {
-    let mn = 0;
-    mn = parseFloat(document.getElementById("m1000").value * 1000) +
-        parseFloat(document.getElementById("m10").value * 10) +
-        parseFloat(document.getElementById("m500").value * 500) +
-        parseFloat(document.getElementById("m5").value * 5) +
-        parseFloat(document.getElementById("m200").value * 200) +
-        parseFloat(document.getElementById("m3").value * 3) +
-        parseFloat(document.getElementById("m100").value * 100) +
-        parseFloat(document.getElementById("m1").value * 1) +
-        parseFloat(document.getElementById("m50").value * 50) +
-        parseFloat(document.getElementById("m0.20").value * 0.2) +
-        parseFloat(document.getElementById("m20").value * 20) +
-        parseFloat(document.getElementById("m0.05").value * 0.05);
+  let mn = 0;
+  mn =
+    parseFloat(document.getElementById("m1000").value * 1000) +
+    parseFloat(document.getElementById("m10").value * 10) +
+    parseFloat(document.getElementById("m500").value * 500) +
+    parseFloat(document.getElementById("m5").value * 5) +
+    parseFloat(document.getElementById("m200").value * 200) +
+    parseFloat(document.getElementById("m3").value * 3) +
+    parseFloat(document.getElementById("m100").value * 100) +
+    parseFloat(document.getElementById("m1").value * 1) +
+    parseFloat(document.getElementById("m50").value * 50) +
+    parseFloat(document.getElementById("m0.20").value * 0.2) +
+    parseFloat(document.getElementById("m20").value * 20) +
+    parseFloat(document.getElementById("m0.05").value * 0.05);
 
-    return mn;
-
+  return mn;
 }
-
 
 function saberusd1() {
-    let usd = 0;
-    usd = parseFloat(document.getElementById("i1000").value * 1000) +
-          parseFloat(document.getElementById("i10").value * 10) +
-          parseFloat(document.getElementById("i500").value * 500) +
-          parseFloat(document.getElementById("i5").value * 5) + 
-          parseFloat(document.getElementById("i200").value * 200) +
-          parseFloat(document.getElementById("i3").value * 3) +
-          parseFloat(document.getElementById("i100").value * 100) +
-          parseFloat(document.getElementById("i1").value * 1) +
-          parseFloat(document.getElementById("i50").value * 50) +
-          parseFloat(document.getElementById("i0.20").value * 0.2) +
-          parseFloat(document.getElementById("i20").value * 20) +
-          parseFloat(document.getElementById("i0.05").value * 0.05);
-          return usd;
-
+  let usd = 0;
+  usd =
+    parseFloat(document.getElementById("i1000").value * 1000) +
+    parseFloat(document.getElementById("i10").value * 10) +
+    parseFloat(document.getElementById("i500").value * 500) +
+    parseFloat(document.getElementById("i5").value * 5) +
+    parseFloat(document.getElementById("i200").value * 200) +
+    parseFloat(document.getElementById("i3").value * 3) +
+    parseFloat(document.getElementById("i100").value * 100) +
+    parseFloat(document.getElementById("i1").value * 1) +
+    parseFloat(document.getElementById("i50").value * 50) +
+    parseFloat(document.getElementById("i0.20").value * 0.2) +
+    parseFloat(document.getElementById("i20").value * 20) +
+    parseFloat(document.getElementById("i0.05").value * 0.05);
+  return usd;
 }
 
+function totalcup1() {
+  return (
+    sabermn1() +
+    saberusd1() * parseFloat(document.getElementById("tasa2").value)
+  );
+}
 
-function totalcup1(){
-  return  sabermn1() + saberusd1() * parseFloat(document.getElementById("tasa2").value);                    
+function totalusd1() {
+  return (
+    saberusd1() +
+    sabermn1() / parseFloat(document.getElementById("tasa2").value)
+  );
+}
 
-};
-
-
-function totalusd1(){
-    return  saberusd1() + sabermn1() /  parseFloat(document.getElementById("tasa2").value);             
-  
-  };
-
-
-  function tasatoque(){
-   document.getElementById("tasa2").value ="";
-  }
+function tasatoque() {
+  document.getElementById("tasa2").value = "";
+}
